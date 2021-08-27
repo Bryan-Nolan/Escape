be050300ed0220c2428880b99a5d5195592a535a
Bryan-Nolan <bryannolan74@gmail.com>
2021-08-27 16:38:16 +0000

    Keypad working and sending to Display 1 digit at time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,91 +1,94 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener("click", function() {
+        button.addEventListener("click", function () {
             if (this.getAttribute("class") === "story") {
                 alert(`Story Key Pressed !`);
             } else if (this.getAttribute("class") === "rules") {
                 alert(`Rules Key Pressed !`);
             } else {
                 let gameLevel = 1;
-                runGame (gameLevel);
+                runGame(gameLevel);
             }
         });
     }
 
-    document.getElementsByClassName('inputKey').addEventListener("keydown", function(event) {
-            alert(`Key Pressed !`);
-        });
+    document.getElementsByClassName('inputKey')[0].addEventListener("click", function (event) {
+        var keyPress = 1;
+        displayGuess(keyPress);
+    });
 
- //   runGame(gameLevel = 1);
+    document.getElementsByClassName('inputKey')[1].addEventListener("click", function (event) {
+        var keyPress = 2;
+        displayGuess(keyPress);
+    });
+
+    document.getElementsByClassName('inputKey')[2].addEventListener("click", function (event) {
+        var keyPress = 3;
+        displayGuess(keyPress);
+    });
+
+    document.getElementsByClassName('inputKey')[3].addEventListener("click", function (event) {
+        var keyPress = 4;
+        displayGuess(keyPress);
+    });
+
+    document.getElementsByClassName('inputKey')[4].addEventListener("click", function (event) {
+        var keyPress = 5;
+        displayGuess(keyPress);
+    });
+
+    document.getElementsByClassName('inputKey')[5].addEventListener("click", function (event) {
+        var keyPress = 6;
+        displayGuess(keyPress);
+    });
+
+    document.getElementsByClassName('inputKey')[6].addEventListener("click", function (event) {
+        var keyPress = 7;
+        displayGuess(keyPress);
+    });
+
+    document.getElementsByClassName('inputKey')[7].addEventListener("click", function (event) {
+        var keyPress = 8;
+        displayGuess(keyPress);
+    });
+
+    document.getElementsByClassName('inputKey')[8].addEventListener("click", function (event) {
+        var keyPress = 9;
+        displayGuess(keyPress);
+    });
+
+    document.getElementsByClassName('inputKey')[9].addEventListener("click", function (event) {
+        var keyPress = 10;
+        displayGuess(keyPress);
+    });
+
+    document.getElementsByClassName('inputKey')[10].addEventListener("click", function (event) {
+        var keyPress = 0;
+        displayGuess(keyPress);
+    });
+
+    document.getElementsByClassName('inputKey')[11].addEventListener("click", function (event) {
+        var keyPress = 12;
+        displayGuess(keyPress);
+    });
+
+
+    //   runGame(gameLevel = 1);
 
 });
 
 /**
- * The main game "loop", called when the script is first loaded
- * and after the user's answer has been processed
+ * Enters Digits into input box as pressed on the keypad
  */
 
-function runGame () {
+ function displayGuess(keyPress) {
+    guess = 0;
+    guess += keyPress;
+    document.getElementById("displayInput").value = guess;
 
-    let num1 = Math.floor(Math.random() * 10);
-    let num2 = Math.floor(Math.random() * 10);
-    let num3 = Math.floor(Math.random() * 10);
-    let num4 = Math.floor(Math.random() * 10);
 }
-
-/**
- * Pop up box which will display rules of the game
- */
-
-function displayRules ()
-
-/**
- * Pop up box which will display story of the game
- */
-
- function displayStory () 
-
-/**
- * Enters Digits into inpox as pressed on the keypad
- */
-
-function displayGuess ()
-
-/**
- * Enters Digits into inpox as pressed on teh keypad
- */
-
-function inputResult ()
-
-/**
- * When next guess is being entered previos guess and resuly will be stored in ordered list
- */
-
- function displayOldGuess ()
-
- /**
- * Clicking the hint button will give you a correct number and position and will remove 3 guesses
- */
-
-function hint ()
-
-
- /**
- * After completeing 1st levek numbe of allowed guesses will decrease by 5
- * There will be 3 levels 20, 15, 10 guesses
- */
-
- function levelUp ()
-
- /**
- * Pop up box which will dislay if you have won or failed 
- */
-
-function endGame ()
-
-
